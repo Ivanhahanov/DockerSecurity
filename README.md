2a0f789a5de442353511a7d161949b21b11a3244
Ivanhahanov <ivanhahanov13@gmail.com>
2020-11-14 21:14:18 +0300

    add reverse function to convert image history to Dockerfile

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # DockerSecurity
 ### Example of usage
+
+##### Check image and container 
 ```bash
-ivan@dockersec:~$  python3 docker_check.py -i nginx -c confident_ellis  --info
+ivan@dockersec:~$ python3 docker_check.py -i nginx -c confident_ellis  --info
 Docker Image Warnings:
 [Users]:        No user in image
 Docker Container Info:
@@ -14,4 +16,22 @@ Docker Container Info:
 - Capabilities: You have no dropped capabilities
 Please follow this link, to read more:
 https://docs.docker.com/engine/reference/run/#runtime-privilege-and-linux-capabilities
+```
+##### Reverse image to dockerfile"
+```bash
+ivan@dockersec:~$ python3 docker_check.py -r nginx
+ ADD file:0dc53e7886c35bc21ae6c4f6cedda54d56ae9c9e9cd367678f1a72e68b3c43d4 in /
+ CMD ["bash"]
+ LABEL maintainer=NGINX Docker Maintainers <docker-maint@nginx.com>
+ ENV NGINX_VERSION=1.19.4
+ ENV NJS_VERSION=0.4.4
+ ENV PKG_RELEASE=1~buster
+ RUN set -x 
+        && addgroup --system --gid 101 nginx
+        ...
+
+ ENTRYPOINT ["/docker-entrypoint.sh"]
+ EXPOSE 80
+ STOPSIGNAL SIGTERM
+ CMD ["nginx" "-g" "daemon off;"]
 ```
--- docker_check.py
+++ docker_check.py
@@ -3,12 +3,15 @@ from source.images import CheckImage
 from source.check import CheckContainer
 from source.info import show_info
 import argparse
+from pprint import pprint
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-i', '--image',
-                    help='Enter Image Name')
+                    help='Image Name')
 parser.add_argument('-c', '--container',
-                    help='Enter Container name')
+                    help='Container name')
+parser.add_argument('-r', '--reverse',
+                    help='Reverse image to Dockerfile')
 parser.add_argument('--info', action="store_true",
                     help='Show info about output result')
 
@@ -22,8 +25,19 @@ def print_result(**kwargs):
         print(f'[{key}]:  \t{value}')
 
 
-if args.image is None and args.container is None:
-    parser.print_help()
+def reverse_docker_image_to_dockerfile(image_name):
+    history = client.history(image_name)
+    for line in reversed(history):
+        line = line['CreatedBy']
+        line = ' '.join(line.split())
+        line = line.replace('/bin/sh -c #(nop)', '')
+        line = line.replace('/bin/sh -c', ' RUN')
+        line = line.replace('&&', "\n\t&&")
+        print(line)
+
+
+if args.reverse:
+    reverse_docker_image_to_dockerfile(args.reverse)
 
 if args.image:
     # Docker Images
@@ -44,7 +58,7 @@ if args.container:
     if args.container in container_names_with_id.keys():
         container_id = container_names_with_id[args.container]
         check_container = CheckContainer(client.inspect_container(container_id))
-        # pprint(client.inspect_container(container_id))
+        pprint(client.inspect_container(container_id))
         print('Docker Container Info:')
         container_result = check_container.check_container()
         print_result(**container_result)
